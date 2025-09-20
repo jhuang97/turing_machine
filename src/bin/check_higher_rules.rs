1729e7c2f8f5623d4970e0feaea579a047f456f7
Jeffrey Huang <jeffreyhuang97@hotmail.com>
2025-09-20 08:18:53 -0500

    bb6_tm4: implement stride, fix code comments on rules

--- src/bin/check_higher_rules.rs
+++ src/bin/check_higher_rules.rs
@@ -811,9 +811,9 @@ fn generate_simulator_code(tm_def: String, state_table: Vec<Rc<Metastate>>, symb
 }
 
 fn main() {
-    // let fname = "src/definitions/bb6_tm_4_counter.txt";
+    let fname = "src/definitions/bb6_tm_4_counter.txt";
     // let fname = "src/definitions/bb6_sk1like_1.txt";
-    let fname = "src/definitions/bb6_sk1like_3.txt";
+    // let fname = "src/definitions/bb6_sk1like_3.txt";
     // let fname = "src/definitions/skelet1/skelet1_reimpl.txt";
 
     let (tm_def, tm, state_table, symbol_table, checked_rules) = 
