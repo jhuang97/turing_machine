8a3a8a7b2c2ac240c71361006245cbb93937e054
Jeffrey Huang <jeffreyhuang97@hotmail.com>
2026-02-06 09:06:20 -0500

    BB(6) class 1 counter

--- src/bin/check_block_rules.rs
+++ src/bin/check_block_rules.rs
@@ -960,7 +960,7 @@ fn generate_simulator_code(tm_def: String, state_table: Vec<Rc<Metastate>>, symb
 }
 
 fn main() {
-    let fname = "src/definitions/bb6_st_counter_bouncer.txt";
+    let fname = "src/definitions/bb6_cl1counter.txt";
 
     let (tm_def, tm, state_table, symbol_table, checked_rules) = 
         process_tm_block_file(fname, CheckerVerbosity::All);
